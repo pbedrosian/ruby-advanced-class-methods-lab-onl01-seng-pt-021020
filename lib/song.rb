a3e7c3a5116e92bd7f12254f6f6b338eeb128e2f
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-03-03 00:35:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,10 +18,10 @@ class Song
     self.class.all << self
   end
 
-  def new_by_name(test)
-    test = self.new
-    test.name = name
-    test
-  end
+  def self.new_by_name(name)
+      song = self.new
+      song.name = name
+      song
+    end
 
 end
